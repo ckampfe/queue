aab125380b030cf155f08bc7d430203f6d40e834
Clark Kampfe <clark.kampfe@gmail.com>
2026-08-10 14:57:49 -0500

    add docs

--- lib/queue.ex
+++ lib/queue.ex
@@ -34,12 +34,26 @@ defmodule Queue do
     extend_back_impl(queue, list)
   end
 
+  @doc """
+  Add a list of terms to the end of the queue atomically.
+  Terms are added in order.
+
+  You should almost always prefer using this function over `push_front/2`.
+
+  You should especially prefer using this function over `push_front/2` if you
+  are adding many items to the queue, as it is optimized
+  for this specific use case, and is often ~10x faster than `push_front/2`.
+
+  This function acquires a lock 1 time, regardless of how many items you add.
+  `push_front/2` acquires a lock to add only a single item, so calling it in
+  a hot loop results in many repeated lock acquisitions.
+  """
   def extend_front(queue, list) when is_list(list) do
     extend_front_impl(queue, list)
   end
 
   @doc """
-  Remove up to the first `n` items from the queue, or an empty list
+  Remove up to `n` items from the front of the queue, or an empty list
   if the queue is empty.
 
   Returns less than `n` items if `len(queue)` < `n`.
@@ -60,15 +74,26 @@ defmodule Queue do
   Add a single term to the end of the queue.
 
   This function is convenient for adding a single item,
-  but you should almost always prefer using `extend/2`,
+  but you should almost always prefer using `extend_back/2`,
   as it is ~10x faster if you are adding multiple items.
 
   This function acquires a lock to add a single item,
-  whereas `extend/2` acquires a lock a single time to add
+  whereas `extend_back/2` acquires a lock a single time to add
   an arbitrary number of items, resulting in much less lock contention.
   """
   def push_back(_queue, _term), do: :erlang.nif_error(:nif_not_loaded)
 
+  @doc """
+  Add a single term to the front of the queue.
+
+  This function is convenient for adding a single item,
+  but you should almost always prefer using `extend_front/2`,
+  as it is ~10x faster if you are adding multiple items.
+
+  This function acquires a lock to add a single item,
+  whereas `extend_front/2` acquires a lock a single time to add
+  an arbitrary number of items, resulting in much less lock contention.
+  """
   def push_front(_queue, _term), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
@@ -81,17 +106,31 @@ defmodule Queue do
     raise "todo"
   end
 
+  @doc """
+  View the item at the front of the queue if the queue is not empty,
+  `nil` if it is.
+
+  Does not modify the queue.
+  """
   def peek_front(_queue), do: :erlang.nif_error(:nif_not_loaded)
 
+  @doc """
+  View the item at the back of the queue if the queue is not empty,
+  `nil` if it is.
+
+  Does not modify the queue.
+  """
   def peek_back(_queue), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Returns the contents of the queue as a list.
+  Does not modify the queue.
   """
   def to_list(_queue), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Returns the length of the queue.
+  Does not modify the queue.
   """
   def count(_queue), do: :erlang.nif_error(:nif_not_loaded)
 
